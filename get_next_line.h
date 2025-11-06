b5f7d2e78e69dddade3ec71896414924c1770cbe
Charbel El Hajj <charbel.elhajj@student.uclouvain.be>
2025-11-06 22:09:07 +0100

    Adding Mandatory Part

---
+++ get_next_line.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cel-hajj <cel-hajj@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 23:00:07 by cel-hajj          #+#    #+#             */
+/*   Updated: 2025/11/06 22:04:25 by cel-hajj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+/* ********* */
+/* Libraries */
+/* ********* */
+
+# include <unistd.h>
+# include <stdlib.h>
+
+/* ************************** */
+/* Buffer Set-Up and Handling */
+/* ************************** */
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/* ********* */
+/* Functions */
+/* ********* */
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_find(char const *s, char c);
+char	*extract_line(char *buffer);
+char	*clear_buffer(char *buffer);
+char	*ft_strdup(const char *s1);
+char	*ft_error(char *buffer);
+size_t	ft_strlen(char const *s);
+
+/* *************** */
+/* Bonus Functions */
+/* *************** */
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cel-hajj <cel-hajj@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 23:10:24 by cel-hajj          #+#    #+#             */
+/*   Updated: 2025/11/06 21:59:59 by cel-hajj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_concat(char const *s1, char const *s2, char *concat)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (s1[j])
+	{
+		concat[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		concat[i] = s2[j];
+		i++;
+		j++;
+	}
+	concat[i] = '\0';
+	return (concat);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dst;
+	size_t	i;
+
+	i = 0;
+	dst = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (!dst)
+		return (NULL);
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*js;
+
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	js = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!js)
+		return (NULL);
+	return (ft_concat(s1, s2, js));
+}
+
+int	ft_find(char const *s, char c)
+{
+	int	i;
+
+	if (!s)
+		return (-1);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+size_t	ft_strlen(char const *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
